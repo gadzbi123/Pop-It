ca870f4f13ec94974fd572f117d4fa2af8defe81
Gadzbi <crg1234@gmail.com>
2023-05-19 14:25:47 +0200

    make a presentable static, started nav for mobile

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
+import "./styles/vars.css";
+import "./styles/reset.css";
+import "./styles/header.css";
+import "./styles/navigation.css";
+import "./styles/button.css";
+import "./styles/margins.css";
+import "./styles/about.css";
+import "./styles/products.css";
+import "./styles/motivation.css";
+import "./styles/footer.css";
+import "./styles/media-queries.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+import "./assets/img/amongus.png";
+import "./assets/img/background.png";
+import "./assets/img/background2.png";
+import "./assets/img/bg-grid.png";
+import "./assets/img/bg-grid2.png";
+import "./assets/img/photo.jpg";
+import "./assets/img/round.png";
+import "./assets/img/square.png";
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".nav__list").classList.add("nav__list--hidden");
 };
